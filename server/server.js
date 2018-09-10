2fc326482349b9e6fbb91d48bfdb0af8ecef33d6
Max <maxwiese13@gmail.com>
2018-09-10 17:28:52 +0200

    some testing

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ let server = net.createServer((socket) => {
     socket.setEncoding('utf8')
 
     socket.on('data', (data) => {
-      //try {
+      try {
         data = JSON.parse(data)
         if (data.hasOwnProperty('Message')) {
           broadcast(data)
@@ -17,9 +17,9 @@ let server = net.createServer((socket) => {
           console.log(data)
         }
 
-      //} catch (e) {
-      //  throw e
-      //}
+      } catch (e) {
+        throw e
+      }
     })
 })
 
@@ -41,6 +41,6 @@ server.listen(3000, '127.0.0.1', () => {
 
 function broadcast(message) {
     clients.map((client) => {
-        client.write(JSON.stringify(message))
+        client.write(JSON.stringify(message)
     })
 }
