a3c253e4682cf451a87298ba36736e5721d83338
Max <maxwiese13@gmail.com>
2018-09-10 20:19:33 +0200

    adding basic movement handler

--- server/server.js
+++ server/server.js
@@ -1,46 +1,90 @@
 const net = require('net')
+const player_actions = require('./player_actions')
 
 let clients = []
+let players = {}
 
+//create the server
 let server = net.createServer((socket) => {
-    console.log(socket.remoteAddress+ ':' +socket.remotePort)
-    clients.push(socket)
+  let socket_address = `${socket.remoteAddress}${socket.remotePort}`
 
-    socket.setEncoding('utf8')
+  //print out client adress
+  console.log(`new client: ${socket_address}`)
 
-    socket.on('data', (data) => {
-      try {
-        data = JSON.parse(data)
-        if (data.hasOwnProperty('Message')) {
-          broadcast(data)
-        } else {
-          console.log(data)
-        }
+  //adding socket to clientlist
+  clients.push(socket)
 
-      } catch (e) {
-        throw e
+  //adding player to global obj using address as key
+  players[socket_address] = {}
+
+  //set the datatype to utf-8
+  socket.setEncoding('utf8')
+
+  //handle disconnect event
+  socket.on('close', () => {
+    console.log("exited")
+  })
+
+  //init eventhandler for some game relevant events
+  socket.on('data', (data) => {
+    try {
+
+      //parsing incomming data into Json
+      data = JSON.parse(data)
+
+      if (data.hasOwnProperty('message')) {
+        //handle Message event
       }
-    })
-})
+      if (data.hasOwnProperty('setname')) {
+        //handle setname event
+        player_name = data['setname']
+        players[socket_address]["name"] = player_name
+        //broadcast(`{"player": "${playername} joined the game"}`)
+      }
+      if (data.hasOwnProperty('join')) {
+        //give the player start coordinates
+        players[socket_address]["x"] = 50
+        players[socket_address]["y"] = 50
+      }
+      if (data.hasOwnProperty('move')) {
+        //handle move from player, direction is given in data section
+        direction = JSON.parse(player_actions.move(data['move']))
+        players[socket_address][Object.keys(direction)] += parseInt(direction[Object.keys(direction)])
+      }
+      if (data.hasOwnProperty('update')) {
+        //send updatet data to all clients
+        broadcast(players)
+      }
 
-server.on('error', (error) => {
-    if (error.code === 'EADDRINUSE') {
-        console.log('Host address is in use, retryning ...')
-        setTimeout(() => {
-            server.close()
-            server.listen(3000, '127.0.0.1', () => {
-                console.log('server on', server.address())
-            })
-        }, 1000)
+    } catch (e) {
+      throw e
     }
+  })
 })
 
+// errorhandling
+server.on('error', (error) => {
+
+  //handle error if port in use (restart server every secound)
+  if (error.code === 'EADDRINUSE') {
+    console.log('Host address is in use, retryning ...')
+    setTimeout(() => {
+      server.close()
+      server.listen(3000, '127.0.0.1', () => {
+        console.log('server on', server.address())
+      })
+    }, 1000)
+  }
+})
+
+//start the server
 server.listen(3000, '127.0.0.1', () => {
-    console.log('server on ',server.address())
+  console.log('server on ', server.address())
 })
 
+//function to broadcast to all players
 function broadcast(message) {
-    clients.map((client) => {
-        client.write(JSON.stringify(message)
-    })
+  clients.map((client) => {
+    client.write(JSON.stringify(message))
+  })
 }
