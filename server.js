bba79f4af96d2becae210cb2dfea37a702bc52ee
Markus Hölzle <markus.hoelzle@multivac.de>
2018-09-10 14:36:33 +0200

    adding more features, basic error handling

--- server.js
+++ server.js
@@ -5,14 +5,29 @@ clients = []
 let server = net.createServer((socket) => {
     console.log(socket.remoteAddress+ ':' +socket.remotePort)
     clients.push(socket)
-    socket.write(`Hello World`)
+    
     socket.setEncoding('utf8')
+    
     socket.on('data', (data) => {
         console.log(data)
         broadcast(data)
     })
-}).listen(3000, '127.0.0.1', () => {
-    console.log(`Server listen to ${server.address()}`)
+})
+
+server.on('error', (error) => {
+    if (error.code === 'EADDRINUSE') {
+        console.log('Host address is in use, retryning ...')
+        setTimeout(() => {
+            server.close()
+            server.listen(3000, '127.0.0.1', () => {
+                console.log('server on', server.address())
+            })
+        }, 1000)
+    }
+})
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('server on ',server.address())
 })
 
 function broadcast(message) {
