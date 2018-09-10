7bd7a7fb0222108d39b00c257d570b531177f2db
Matthias <42836408+AdmiralCanelli@users.noreply.github.com>
2018-09-10 17:31:34 +0200

    TEST

--- server/server.js
+++ server/server.js
@@ -3,44 +3,44 @@ const net = require('net')
 let clients = []
 
 let server = net.createServer((socket) => {
-    console.log(socket.remoteAddress+ ':' +socket.remotePort)
-    clients.push(socket)
+  console.log(socket.remoteAddress + ':' + socket.remotePort)
+  clients.push(socket)
 
-    socket.setEncoding('utf8')
+  socket.setEncoding('utf8')
 
-    socket.on('data', (data) => {
-      try {
-        data = JSON.parse(data)
-        if (data.hasOwnProperty('Message')) {
-          broadcast(data)
-        } else {
-          console.log(data)
-        }
-
-      } catch (e) {
-        throw e
+  socket.on('data', (data) => {
+    try {
+      data = JSON.parse(data)
+      if (data.hasOwnProperty('Message')) {
+        broadcast(data)
+      } else {
+        console.log(data)
       }
-    })
+
+    } catch (e) {
+      throw e
+    }
+  })
 })
 
 server.on('error', (error) => {
-    if (error.code === 'EADDRINUSE') {
-        console.log('Host address is in use, retryning ...')
-        setTimeout(() => {
-            server.close()
-            server.listen(3000, '127.0.0.1', () => {
-                console.log('server on', server.address())
-            })
-        }, 1000)
-    }
+  if (error.code === 'EADDRINUSE') {
+    console.log('Host address is in use, retryning ...')
+    setTimeout(() => {
+      server.close()
+      server.listen(3000, '127.0.0.1', () => {
+        console.log('server on', server.address())
+      })
+    }, 1000)
+  }
 })
 
 server.listen(3000, '127.0.0.1', () => {
-    console.log('server on ',server.address())
+  console.log('server on ', server.address())
 })
 
 function broadcast(message) {
-    clients.map((client) => {
-        client.write(JSON.stringify(message)
-    })
-}
+  clients.map((client) => {
+      client.write(JSON.stringify(message)
+      })
+  }
