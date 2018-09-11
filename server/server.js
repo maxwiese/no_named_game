936cb92f3db0f71fe8e98933f53784fd374940b8
Max <maxwiese13@gmail.com>
2018-09-11 17:14:03 +0200

    edit

--- server/server.js
+++ server/server.js
@@ -90,16 +90,3 @@ server.on('error', (error) => {
 server.listen(3000, '127.0.0.1', () => {
   console.log('server on ', server.address())
 })
-<<<<<<< HEAD
-
-//<<<<<<< HEAD
-function broadcast(message) {
-  clients.map((client) => {
-      client.write(JSON.stringify(message));
-      })
-  }
-//=======
-
-//>>>>>>> master
-=======
->>>>>>> edit
