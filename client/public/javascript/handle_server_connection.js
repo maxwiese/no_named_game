9cf29c70aab4626ac220016ca288bef1ea465e8e
Markus Hölzle <markus.hoelzle@multivac.de>
2018-09-17 09:41:18 +0200

    changed how to connect to the server

---
+++ client/public/javascript/handle_server_connection.js
@@ -0,0 +1,27 @@
+const { ipcRenderer } = require('electron')
+
+function connect() {
+    ipcRenderer.send('connect')
+}
+
+function send(command, data) {
+    ipcRenderer.send('send', {'command': command, 'data': data, 'timestamp': Date.now()})
+}
+
+function disconnect() {
+    ipcRenderer.send('disconnect')
+}
+
+ipcRenderer.on('connected', (event, args) => {
+    console.log(`client connected`)
+})
+
+ipcRenderer.on('recived', (event, args) => {
+    if ('timestamp' in args) {
+        let recived = Date.now()
+        let sended = parseInt(args['timestamp'])
+        let delay = recived - sended
+        console.log(`delay : ${delay}ms`)
+    }
+    handle_response(args)
+})
