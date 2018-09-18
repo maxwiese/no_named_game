6f9f0a721eb55effe10bfc96b39159062a9ad77d
Markus Hölzle <markus.hoelzle@multivac.de>
2018-09-18 13:15:57 +0200

    list all players in lobby

--- client/public/javascript/handle_server_response.js
+++ client/public/javascript/handle_server_response.js
@@ -1,15 +1,30 @@
 function handle_response(data) {
-    console.log(data)
+   // console.log(data)
     if ('timestamp' in data) {
         let recived = Date.now()
         let sended = parseInt(data['timestamp'])
         let delay = recived - sended
         document.getElementById('ping'). innerHTML = delay + ' ms'
     }
-/*     for (obj in data) {
-        if (data[obj].hasOwnProperty('name')) {
-            cube.position.set(data[obj].x/10, data[obj].y/10, 1)
-            console.log(cube.position)
-        }        
-    } */
+    keys = Object.keys(data)
+    player_ips = keys.slice(0, keys.length-1)
+    if (data[player_ips].name == player_name) {
+        if ('state' in data[player_ips]) {
+            if (data[player_ips].state == 'mainmenu') {
+
+            } else if (data[player_ips].state == 'lobby') {
+                for(player of player_ips) {
+                    let playeratdom = document.getElementById(data[player].name)
+                    if (playeratdom === null){
+                        let tr = document.createElement('tr')
+                        tr.innerHTML = `<td id="${data[player].name}">${data[player].name}</td>`
+                        document.getElementById('players').appendChild(tr)
+                    }
+                }
+
+            } else if (data[player_ips].state == 'game') {
+
+            }
+        }
+    }
 }
