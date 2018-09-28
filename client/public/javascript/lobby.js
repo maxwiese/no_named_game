e31f8fc05d8ae4699f0f71987fa10b97dd0b48c8
Matthias Pennig <matthias.pennig@multivac.de>
2018-09-28 11:05:58 +0200

    Sort Files, styled Settings PopUp

---
+++ client/public/javascript/lobby.js
@@ -0,0 +1,25 @@
+var config = {
+    type: Phaser.Auto,
+    width: 300,
+    height: 650,
+    parent: 'character',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    }
+}
+
+var game = new Phaser.Game(config)
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
